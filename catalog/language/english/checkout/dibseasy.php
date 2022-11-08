94c8bd7b729096d25b6d06a896fb63233ead8af7
Mehabub Husen Shaikh <mehabub.shaikh@external.nexigroup.com>
2022-11-08 15:01:04 +0530

    For payment not initialised issue

---
+++ catalog/language/english/checkout/dibseasy.php
@@ -0,0 +1,15 @@
+<?php
+
+$_['button_checkout_label'] = 'Use other payment method';
+$_['order_summary_label'] = 'Order Summary';
+$_['shipping_methods_label'] = 'Shipping Methods';
+
+$_['shipping_total_label'] = 'Shipping';
+
+$_['dibseasy_checkout_redirect_error'] = 'Error during payment initialization, try anoter payment method';
+
+$_['totals_tax_label'] = 'Taxes';
+
+$_['totals_subtotal_label'] = 'Subtotal';
+
+$_['totals_subtotal_shipping'] = 'Shipping';
